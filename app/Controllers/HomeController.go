8676e5aab6829f0c1611b8b088df8187690c050f
DrLenux <drlenux@gmail.com>
2017-07-14 23:54:33 +0300

    init

---
+++ app/Controllers/HomeController.go
@@ -0,0 +1,5 @@
+package Controllers
+
+func Run() string {
+	return "<center><h1>Hello</h1></center>"
+}
---
+++ site.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/go-martini/martini"
+import "github.com/drlenux/go/app/Controllers/HomeController"
+//import "github.com/codegangsta/inject"
+
+func main() {
+	m := martini.Classic()
+	m.Get("/", func () string {
+		return HomeController.Run();
+	})
+	m.Run()
+}
